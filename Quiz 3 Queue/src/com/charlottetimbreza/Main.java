e0238925e9577a9171d3270b027ccdb8a53ba833
Charlotte Timbreza <charlotte.timbreza@benilde.edu.ph>
2021-11-03 13:46:51 +0800

    Quiz 3 Submission

---
+++ Quiz 3 Queue/src/com/charlottetimbreza/Main.java
@@ -0,0 +1,55 @@
+package com.charlottetimbreza;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayQueue matchmakingQueue = new ArrayQueue(7);
+        int gameStarted = 0;
+
+        while (gameStarted < 10) {
+            addToMatchmaking(matchmakingQueue);
+            printPauseTurn();
+            if(matchmakingQueue.size() >= 5)
+                startGame(gameStarted, matchmakingQueue);
+            System.out.println("\nMatchmaking Queue");
+            matchmakingQueue.printQueue();
+            System.out.print("\n");
+        }
+
+    }
+
+    public static void printPauseTurn() {
+        System.out.println("Press enter to end the turn and start a new one");
+        Scanner pause = new Scanner(System.in);
+        pause.nextLine();
+    }
+
+    public static int rng(int min, int max) {
+        Random random = new Random();
+        return random.nextInt(((max - min) + 1) + min);
+    }
+
+    public static void addToMatchmaking(ArrayQueue arrayQueue) {
+        int randNumber = rng(1, 7);
+        System.out.println("Added players to matchmaking queue: ");
+        for (int i = 0; i < randNumber; i++) {
+            int randPlayerId = rng(0, 200);
+            Player currentPlayer = new Player(randPlayerId);
+            arrayQueue.add(currentPlayer);
+            System.out.println(currentPlayer);
+        }
+    }
+
+    public static void startGame(int gameCounter, ArrayQueue arrayQueue) {
+        gameCounter++;
+        System.out.println("Game " + gameCounter + " has started with the following players!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arrayQueue.peek());
+            arrayQueue.remove();
+
+        }
+    }
+}
---
+++ Quiz 3 Queue/src/com/charlottetimbreza/Player.java
@@ -0,0 +1,39 @@
+package com.charlottetimbreza;
+
+import java.util.Objects;
+
+public class Player {
+    private int playerId;
+
+    public Player(int name) {
+        this.playerId = name;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name=" + playerId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return playerId == player.playerId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerId);
+    }
+}
