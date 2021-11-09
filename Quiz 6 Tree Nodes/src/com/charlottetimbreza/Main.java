d26c7647e0b2000b7eda2bf8e2c41ba67b72d673
Charlotte Timbreza <charlotte.timbreza@benilde.edu.ph>
2021-11-09 14:42:23 +0800

    Quiz 6 Submission

---
+++ Quiz 6 Tree Nodes/src/com/charlottetimbreza/Main.java
@@ -0,0 +1,24 @@
+package com.charlottetimbreza;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Tree tree = new Tree();
+
+        tree.insert(25);
+        tree.insert(17);
+        tree.insert(29);
+        tree.insert(10);
+        tree.insert(16);
+        tree.insert(-5);
+        tree.insert(60);
+        tree.insert(55);
+        tree.insert(-83);
+
+        //tree.traverseInOrder();
+        tree.traverseInOrderDescending();
+        //System.out.println(tree.get(55));
+        //System.out.println(tree.getMax());
+    }
+}
